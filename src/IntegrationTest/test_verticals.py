a930b4dc4240f9ebd69beee2f9114c4297737484
shaaarp legs <450632@student.fontys.nl>
2022-11-22 02:38:55 +0100

    test generated id test

--- src/IntegrationTest/test_verticals.py
+++ src/IntegrationTest/test_verticals.py
@@ -3,11 +3,27 @@ import json
 import numpy as np
 import pandas as pd
 import pytest
+import os
+import boto3
 
 # Getting all the verticals
 
+cloudformation = boto3.resource(
+    service_name='cloudformation',
+    region_name='eu-west-1',
+    aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"] ,
+    aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"] 
+)
+
+
+def getApiGatewayGeneratedID():
+    for stack in cloudformation.stacks.all():
+        for r in stack.resource_summaries.all() :
+            if (str(r.stack_name)=="aws-ofs-verticals-microservice-dev") & (str(r.logical_id) == 'HttpApi') :
+                return str(r.physical_resource_id)
+
 def getVerticals():
-    return requests.get('https://67xjp60w0e.execute-api.eu-west-1.amazonaws.com/vertical').json()
+    return requests.get('https://{generatedId}.execute-api.eu-west-1.amazonaws.com/vertical'.format(generatedId=getApiGatewayGeneratedID())).json()
 
 
 def test_answer():
