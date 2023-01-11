c1d6dabfad5bb1cabe8866f98eb96ba91ab80372
shaaarp legs <450632@student.fontys.nl>
2023-01-11 19:13:43 +0100

    github workflows updated

--- src/react-ofs-frontend/cypress/e2e/login.cy.js
+++ src/react-ofs-frontend/cypress/e2e/login.cy.js
@@ -14,15 +14,16 @@ it("Go to the login page", ()=> {
 
 
 it("Go to the login page", ()=> {
+    // local frontend version in vm machine / local machine
     cy.visit('http://localhost:3000/')
     cy.get('[style="opacity: 1; order: 1;"]').click()
     cy.origin('https://ofs-auth.auth.eu-west-1.amazoncognito.com', () => {
         cy.get('#signInFormUsername').type(Cypress.env().username, {force: true})
         cy.get('#signInFormPassword').type(Cypress.env().password, {force: true})
-        // cy.get('input[value="Sign in"]').click({ multiple: true })
         cy.get('input[name="signInSubmitButton"]').click({ multiple: true, force:true })
     })
-    cy.origin('http://localhost:3000/', () => {
+    // after login, the user should be redirected to the home page
+    cy.origin('https://ofs-platform.com/', () => {
         cy.get('span[class="ant-breadcrumb-link"]') == 'Freelancer / Welcome, Mohammed Saleh Said Salem Alharbi'
     })
 })
