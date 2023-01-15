cddd7fc4bd2c52e1e0bc5e1683d90ad14730e401
shaaarp legs <450632@student.fontys.nl>
2023-01-15 13:38:19 +0100

    pricingEstimator now can predict, re-train on new data and get possible predictable FS prices

---
+++ src/aws-ofs-pricingEstimator-AI/app.py
@@ -0,0 +1,24 @@
+## Create a MongoDB client, open a connection to Amazon DocumentDB as a replica set and specify the read preference as secondary preferred 
+                    
+import pymongo
+import sys
+import pandas as pd
+
+client = pymongo.MongoClient('mongodb+srv://HarbiTechMoe_MDB:8JKOWD8Q6V1Of0Xg@aws-ofs-pricingdb.2nunmyc.mongodb.net')
+
+##Specify the database to be used
+db = client.pricing
+
+##Specify the collection to be used
+col = db.pricing
+
+cursor = col.find({})
+df = pd.DataFrame(cursor)
+
+##Count the number of records in the collection
+fsCount = pd.DataFrame(df['FS'].value_counts()).reset_index()
+fsCount.columns = ['FS','count']
+print(fsCount[fsCount['count'] > 45]['FS'].tolist())
+
+
+client.close()
--- src/aws-ofs-pricingEstimator/app.py
+++ src/aws-ofs-pricingEstimator/app.py
@@ -2,9 +2,16 @@ from fastapi import FastAPI, Header
 import boto3
 import pickle
 from pydantic import BaseModel
+import pymongo
+import sys
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.feature_extraction.text import CountVectorizer
+import s3fs
+from pickle import dump
+import os
 
 app = FastAPI()
-
 class Prompt(BaseModel):
     description: str
 
@@ -35,3 +42,84 @@ def predict(body_dict: Prompt,
     predicted_price = model.predict(new_description_vector)
 
     return {"Price":int(predicted_price[0])}
+
+@app.get("/predictableFreelancingServices/{threshold}")
+def getPredictableFreelancingServices(
+    username: str = Header(None),
+    password: str = Header(None),
+    threshold: int = 45
+):
+    client = pymongo.MongoClient('mongodb+srv://{user}:{password}@aws-ofs-pricingdb.2nunmyc.mongodb.net'.format(user=username, password=password))
+    ##Specify the database to be used
+    db = client.pricing
+
+    ##Specify the collection to be used
+    col = db.pricing
+
+    cursor = col.find({})
+    df = pd.DataFrame(cursor)
+
+    ##Count the number of records in the collection
+    fsCount = pd.DataFrame(df['FS'].value_counts()).reset_index()
+    fsCount.columns = ['FS','count']
+
+    client.close()
+    return {'freelancingServices':fsCount[fsCount['count'] > threshold]['FS'].tolist()}    
+
+@app.get("/updateModel/{threshold}")
+def updateModel(
+    username: str = Header(None),
+    password: str = Header(None),
+    threshold: int = 45,
+    predictionKeyID: str = Header(None),
+    predictionSecretAccess: str = Header(None)
+):
+    os.environ['AWS_ACCESS_KEY_ID'] = predictionKeyID
+    os.environ['AWS_SECRET_ACCESS_KEY'] = predictionSecretAccess
+    client = pymongo.MongoClient('mongodb+srv://{user}:{password}@aws-ofs-pricingdb.2nunmyc.mongodb.net'.format(user=username, password=password))
+    ##Specify the database to be used
+    db = client.pricing
+
+    ##Specify the collection to be used
+    col = db.pricing
+
+    cursor = col.find({})
+    df = pd.DataFrame(cursor)
+
+    fsCount = pd.DataFrame(df['FS'].value_counts()).reset_index()
+    fsCount.columns = ['FS','count']
+    trainableFS = fsCount[fsCount['count'] > threshold]['FS'].tolist()
+
+    df = df[df['FS'].isin(trainableFS)][['description','price']]
+    df
+    # Vectorize the descriptions using CountVectorizer
+    vectorizer = CountVectorizer()
+    descriptions_vectors = vectorizer.fit_transform(df.description)
+
+    # Train a linear regression model on the vectors
+    regressor = LinearRegression()
+    regressor.fit(descriptions_vectors, df['price'])
+
+    # Save the model to a file
+    with open('model.pkl', 'wb') as f:
+        pickle.dump(regressor, f)
+
+    # Save the vectorizer to a file
+    with open('vectorizer.pkl', 'wb') as f:
+        pickle.dump(vectorizer, f)
+
+
+    fs = s3fs.S3FileSystem(anon=False)
+
+    bucket = 'pricing-estimator-ai'
+    model_path = 'model.pkl'
+    vectorizer_path = 'vectorizer.pkl'
+
+    # update model to s3
+    dump(regressor, fs.open(f's3://{bucket}/{model_path}', 'wb'))
+
+    # update vectorizer to s3
+    dump(vectorizer, fs.open(f's3://{bucket}/{vectorizer_path}', 'wb'))
+
+    client.close()
+    return { 'message': 'Model updated successfully' }
